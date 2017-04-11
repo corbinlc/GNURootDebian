b8501c3fed22b5b4fff262f8bd429ea77a09e54b
MatthewTighe <matthewdtighe@gmail.com>
2017-04-11 14:10:19 -0700

    Server settings are now saved as shared app preferences.

--- GNURootDebianSource/src/main/java/com/gnuroot/debian/GNURootServerSettings.java
+++ GNURootDebianSource/src/main/java/com/gnuroot/debian/GNURootServerSettings.java
@@ -47,75 +47,28 @@ public class GNURootServerSettings extends Activity implements CompoundButton.On
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        SharedPreferences prefs = getSharedPreferences("MAIN", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
         switch(buttonView.getId()) {
             case R.id.launch_ssh_checkbox:
                 Toast.makeText(this, "ssh", Toast.LENGTH_LONG).show();
                 if(!isChecked && !check_vnc.isChecked())
                     check_vnc.setChecked(true);
+                editor.putBoolean("sshLaunch", isChecked);
                 break;
             case R.id.launch_term_checkbox:
-
+                editor.putBoolean("termLaunch", isChecked);
                 break;
             case R.id.launch_vnc_checkbox:
                 Toast.makeText(this, "vnc", Toast.LENGTH_LONG).show();
                 if(!isChecked && !check_ssh.isChecked())
                     check_ssh.setChecked(true);
+                editor.putBoolean("vncLaunch", isChecked);
                 break;
             case R.id.launch_graphical_checkbox:
-
+                editor.putBoolean("graphicalLaunch", isChecked);
                 break;
         }
+        editor.commit();
     }
-
-    /*
-    private void sshBox(boolean isChecked, boolean vncChecked) {
-        if(isChecked) {
-            // TODO shared preferences blabla
-            Toast.makeText(this, "ssh checked", Toast.LENGTH_LONG).show();
-        }
-        else {
-            if(!vncChecked) {
-                vncBox(true, false);
-                // TODO shared preferences blabla
-                Toast.makeText(this, "ssh unchecked, vnc to be checked", Toast.LENGTH_LONG).show();
-            }
-            else {
-                // TODO shared preferences blabla
-                Toast.makeText(this, "ssh unchecked", Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-
-    private void termBox(boolean isChecked) {
-        if(isChecked)
-            Toast.makeText(this, "term checked", Toast.LENGTH_LONG).show();
-        else
-            Toast.makeText(this, "term unchecked", Toast.LENGTH_LONG).show();
-    }
-
-    private void vncBox(boolean isChecked, boolean sshChecked) {
-        if(isChecked) {
-            // TODO shared preferences blabla
-            Toast.makeText(this, "vnc checked", Toast.LENGTH_LONG).show();
-        }
-        else {
-            if(!sshChecked) {
-                vncBox(true, false);
-                // TODO shared preferences blabla
-                Toast.makeText(this, "vnc unchecked, ssh to be checked", Toast.LENGTH_LONG).show();
-            }
-            else {
-                // TODO shared preferences blabla
-                Toast.makeText(this, "vnc unchecked", Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-
-    private void graphBox(boolean isChecked) {
-        if(isChecked)
-            Toast.makeText(this, "graph checked", Toast.LENGTH_LONG).show();
-        else
-            Toast.makeText(this, "graph unchecked", Toast.LENGTH_LONG).show();
-    }
-    */
 }
